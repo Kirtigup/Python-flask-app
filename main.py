5e755d888dc889e01371773e4a7b9b77f03ce174
Kirtigup <75973406+Kirtigup@users.noreply.github.com>
2021-03-08 00:48:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def hello1():
-    return render_template('about.html')
+    return render_template('index.html')
 
 @app.route("/about")
 def hello2():
